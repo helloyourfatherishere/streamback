d877d3b857e49f97682b17292c9a1e5624539d3f
unknown <webworkfirst@gmail.com>
2021-09-19 20:45:09 -0700

    c

--- app.js
+++ app.js
@@ -910,7 +910,7 @@ app.post("/feed/:sub", (req, res)=>{
 })
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/front/build/index.html'));
+    res.sendFile(path.join(__dirname, 'front/build/index.html'));
   });
 
 if(process.env.NODE_ENV === "production"){
