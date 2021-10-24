f2f201377d8a3586ab9b389eb55b9c62f895e2d9
unknown <webworkfirst@gmail.com>
2021-10-23 23:54:05 -0700

    casd

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Origin':'*',
     'Access-Control-Allow-Methods':'GET,HEAD,PUT,PATCH,POST,DELETE',
     'preflightContinue': false,
-    origin : "https://tmwords.netlify.app",
+    origin : "https://tmwords.netlify.appy",
     credentials: true,
   }));
   
@@ -358,8 +358,6 @@ app.post("/signin", (req, res)=>{
                 res.cookie("jwt", token, {
                     httpOnly: true,
                     maxAge: 100* 10* 60 * 60 * 24 * 7 ,
-                    sameSite:"None",
-                    secure:true
                 }).send({status:true});
             }
             else{
