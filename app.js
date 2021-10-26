377a70687717f155d81a3449e3d6dd01be907bf6
unknown <webworkfirst@gmail.com>
2021-10-26 01:11:30 -0700

    commit

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Credentials',"true");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Methods','GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('preflightContinue','false');
     next();
   }) 
 //   app.use(cors({
