f7aa9f78bfbe018ddeed1aab33fc6d08193a64cb
unknown <webworkfirst@gmail.com>
2021-10-27 23:25:54 -0700

    comiiting

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ app.get("/api", (req, res)=>{
         let collections = await product.find({$and: [{category: "collections"}, {brand: false}, {visiblity: true}]}).sort({date: -1}).limit(10);
         let undergarments = await product.find({$and: [{category: "undergarments"}, {brand: false}, {visiblity: true}]}).sort({date: -1}).limit(10);
 
-        res.set("Access-Control-Allow-Origin",req.headers.origin)
+        res.set("Access-Control-Allow-Origin","https://tmwords.netlify.app/")
         res.set('Access-Control-Allow-Credentials',"true")
         res.set('Access-Control-Allow-Headers',"GET,POST,PUT,DELTE")
         res.send([pants,shirts, tShirts,jackets,hoodies,trouser,shalwarKameez,kurti,collections,undergarments, poster, user ,poster]);
