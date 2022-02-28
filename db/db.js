34ab5a3d30434aeaf484294c43b1a2877f947b6d
unknown <webworkfirst@gmail.com>
2022-02-27 22:55:44 -0800

    as

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
 var mongoose= require("mongoose");
-// var db= "mongodb+srv://ecom:ecom123@db.gby5k.mongodb.net/db?retryWrites=true&w=majority"
+// var db= "mongodb://localhost:27017/stream"
 
-var db= "mongodb://localhost:27017/stream"
+var db = "mongodb+srv://stream:webWorkFirst_1104@cluster0.fwcin.mongodb.net/stream?retryWrites=true&w=majority"
 mongoose.connect(db,
     {useNewUrlParser: true,
     useUnifiedTopology: true,
